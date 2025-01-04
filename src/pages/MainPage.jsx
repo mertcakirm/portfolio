2f60ce363c1345faa024da9ec1b859ee3a99c6cd
mertcakirm <mertcakirm3377@gmail.com>
2025-01-04 04:38:22 +0300

    .

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar.jsx";
+import './css/MainPage.css'
+const MainPage = () => {
+    return (
+        <div className="main-page-parent-con">
+            <Navbar />
+        </div>
+    );
+};
+
+export default MainPage;
